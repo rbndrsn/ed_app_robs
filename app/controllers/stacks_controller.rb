0b5073bde9fb356a2e2db0719926e6c953667004
George Gray <george@ummodesign.com>
2013-05-10 00:04:22 +0100

    added JSON format to CRUD

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -14,11 +14,14 @@ class StacksController < ApplicationController
 
   def create
     @stack = Stack.new(params[:stack])
-    if @stack.save
-      redirect_to stacks_path
-    else
-      flash[:error] = "Sorry there was an error saving your Stack"
-      render action: "new"
+    respond_to do |format|
+      if @stack.save
+        format.html { redirect_to @stack, notice: 'stack was successfully created.' }
+        format.json { render json: @stack, status: :created, location: @stack }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @stack.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -26,24 +29,28 @@ class StacksController < ApplicationController
     @stack = Stack.find_by(id:params[:id])
     respond_to do |format|
       format.html { }
-      format.json { render json: @stacks }
+      format.json { render json: @stack }
     end
   end
 
    def edit
     @stack = Stack.find_by(id:params[:id])
     respond_to do |format|
-      format.html
-      format.json { render :json => @stack}
+      format.html { }
+      format.json { render :json => @stack }
     end
   end
 
   def update
     @stack = Stack.find_by(id:params[:id])
-    if @stack.update_attributes(params[:stack])
-      redirect_to @stack
-    else
-      render action: "edit"
+    respond_to do |format|
+      if @stack.update_attributes(params[:stack])
+        format.html { redirect_to @stack, notice: 'stack was successfully updated.' }
+        format.json { render json: @stack, status: :created, location: @stack }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @stack.errors, status: :unprocessable_entity }
+      end
     end
   end
 
